338ac1ae06120674f76f018a914a6bff97513333
Zayeed Chowdhury <zayeed.edureka@gmail.com>
2023-08-24 01:45:19 -0400

    Added Icon and splash screen

--- app/src/main/java/com/example/diary/MainActivity.kt
+++ app/src/main/java/com/example/diary/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.diary
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.widget.TextViewCompat
 import com.example.diary.ui.theme.DiaryTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             DiaryTheme {
 
