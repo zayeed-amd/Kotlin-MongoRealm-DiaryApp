40dd6bd9cf7db9a55f8b5ced1dfaad9b46647594
Zayeed Chowdhury <zayeed.edureka@gmail.com>
2023-08-24 04:03:42 -0400

    Added Google Authentication at Atlas App services via GCP

---
+++ app/src/main/java/com/example/diary/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.diary.util
+
+object Constants{
+    const val APP_ID = "mydairyapp-zachowdh"
+    const val CLIENT_ID = "623706680250-cr866uuaojq21fc75tml7t46on07g07m.apps.googleusercontent.com"
+}
