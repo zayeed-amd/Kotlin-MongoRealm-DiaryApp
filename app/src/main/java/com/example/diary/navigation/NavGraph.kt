665cddc6d9b351b3498f264d953fa5ddc7ea1398
Zayeed Chowdhury <zayeed.edureka@gmail.com>
2023-09-02 16:43:52 -0400

    Setup NavGraph & design auth screen

--- app/src/main/java/com/example/diary/navigation/NavGraph.kt
+++ app/src/main/java/com/example/diary/navigation/NavGraph.kt
@@ -1,5 +1,6 @@
 package com.example.diary.navigation
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
@@ -7,27 +8,36 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.example.diary.presentation.screens.auth.AuthenticationScreen
 import com.example.diary.util.Constants.WRITE_SCREEN_ARG_KEY
 
 @Composable
 fun SetupNavGraph(startDestination: String, navController: NavHostController){
     NavHost(startDestination = startDestination,
         navController = navController){
+        // define all the screens our app will have within this composable function
+        // call all the extension functions here
         authenticationRoute()
         homeRoute()
         writeRoute()
     }
 }
 
-fun NavGraphBuilder.authenticationRoute(){
-    composable(route = Screen.Authentication.route){
+// https://kotlinlang.org/docs/extensions.html
+// extension functions
 
+@OptIn(ExperimentalMaterial3Api::class)
+fun NavGraphBuilder.authenticationRoute(){
+    composable(route = Screen.Home.route){
+        AuthenticationScreen(loadingState = false, onButtonClicked = {})
+        println("authenticationRoute executed")
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 fun NavGraphBuilder.homeRoute(){
     composable(route = Screen.Home.route){
-
+//        AuthenticationScreen(loadingState = false, onButtonClicked = {})
     }
 }
 
